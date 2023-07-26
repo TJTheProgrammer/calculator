c71eb50d40fe488dad0d170421140447acf2ac67
TJTheProgrammer <57811269+TJTheProgrammer@users.noreply.github.com>
2023-07-25 21:44:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+var input1 = null;
+var input2 = "";
+var operator = null;
+
+var decimal = false;
+
+function division() {
+    operation=division;
+}
+
+function digit(num) {
+    input2 = input2.toString() + num;
+    document.getElementById("current").innerHTML = input2;
+}
+
+function decimalPoint() {
+    if (decimal == false) {
+        decimal = true;
+        input2 = input2.toString() + ".";
+        document.getElementById("current").innerHTML = input2;  
+    }
+}
+
+function operation(selection) {
+    if (operator == null && input1 == null) {
+        operator = selection;
+        input1 = input2;
+        input2 = "";
+        document.getElementById("previousandOp").innerHTML = input1 +  " " + operator;
+        document.getElementById("current").innerHTML = input2;
+    
+}
+    else {
+        equality()
+    }
+
+}
+
+function equality() {
+     if (input1 == null || operator == null) {
+
+     }
+     else {
+        switch(operator) {
+            case "+":
+                input2 = parseFloat(input1) + parseFloat(input2);
+                input1 = null;
+                document.getElementById("current").innerHTML = input2;
+                document.getElementById("previousandOp").innerHTML = input1;
+                operator = null;
+            break;
+
+            case "-":
+                input2 = parseFloat(input1) - parseFloat(input2);
+                input1 = null;
+                document.getElementById("current").innerHTML = input2;
+                document.getElementById("previousandOp").innerHTML = input1;
+                operator = null;
+            break;
+                
+
+            case "*":
+                input2 = parseFloat(input1) * parseFloat(input2);
+                input1 = null;
+                document.getElementById("current").innerHTML = input2;
+                document.getElementById("previousandOp").innerHTML = input1;
+                operator = null;
+            break;
+
+            case "÷":
+                input2 = parseFloat(input1) / parseFloat(input2);
+                input1 = null;
+                document.getElementById("current").innerHTML = input2;
+                document.getElementById("previousandOp").innerHTML = input1;
+                operator = null;
+            break;
+        }
+     }
+}
+
+
+function deletion() {
+    input2 = input2.slice(0, -1);
+    document.getElementById("current").innerHTML = input2;
+}
+
+function clearing() {
+    input2 = "";
+    document.getElementById("current").innerHTML = input2;
+}
+
