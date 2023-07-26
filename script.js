b7ddae4b982c59dd3096568ee7e0c4878f262657
TJTheProgrammer <57811269+TJTheProgrammer@users.noreply.github.com>
2023-07-25 22:00:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,92 +1,111 @@
-var input1 = null;
-var input2 = "";
-var operator = null;
+let colorpick = document.getElementById("colorpick");
 
-var decimal = false;
 
-function division() {
-    operation=division;
+let ColRow = document.getElementById("scroller").value;
+
+let currentcolor =  "red"
+
+let tbl = document.getElementById("grid");
+
+for (let i = 0; i < 16; i++) {
+    let myRow = document.createElement("tr");
+    myRow.id = "row"+i;
+    myRow.style.cssText = "onclick = changeCell(this); onmouseover = hoverCellChange(this)";
+    myRow.style.backgroundColor = currentcolor;
+
+    tbl.appendChild(myRow);
+    let rowW = document.getElementById("row"+i);
+
+    for (let j = 0; j < 16; j++) {
+        let myCell = document.createElement("td");
+        myCell.id = "cell"+i+"-"+j;
+        rowW.appendChild(myCell);
+    }
+    [].forEach.call(document.getElementsByTagName('td'), function(item) {
+        item.addEventListener('mousedown', function() {
+            item.style.backgroundColor = currentcolor;
+        }, false)
+        item.addEventListener('mouseover', function() {
+           if (mouseDown == 1) {
+            item.style.backgroundColor = currentcolor;
+           }
+            
+        }, false)
+    })
 }
 
-function digit(num) {
-    input2 = input2.toString() + num;
-    document.getElementById("current").innerHTML = input2;
+function gridChange() {
+ColRow = document.getElementById("scroller").value;
+let newTable = document.getElementById("grid");
+while (newTable.firstChild) {
+    newTable.removeChild(newTable.firstChild);
 }
 
-function decimalPoint() {
-    if (decimal == false) {
-        decimal = true;
-        input2 = input2.toString() + ".";
-        document.getElementById("current").innerHTML = input2;  
+
+for (let i = 0; i < ColRow; i++) {
+    let myRow = document.createElement("tr");
+    myRow.id = "row"+i;
+    myRow.style.cssText = "onmouseenter= changeCell(this); onmouseenter = hoverCellChange(this)";
+    myRow.style.backgroundColor = currentcolor;
+
+    tbl.appendChild(myRow);
+    let rowW = document.getElementById("row"+i);
+
+    for (let j = 0; j < ColRow; j++) {
+        let myCell = document.createElement("td");
+        myCell.id = "cell"+i+"-"+j;
+        rowW.appendChild(myCell);
+    }
+    [].forEach.call(document.getElementsByTagName('td'), function(item) {
+        item.addEventListener('mousedown', function() {
+            item.style.backgroundColor = currentcolor;
+        }, false)
+        item.addEventListener('mouseover', function() {
+            if (mouseDown == 1) {
+             item.style.backgroundColor = currentcolor;
+            }
+             
+         }, false)
+    })
+}
+
+}
+
+
+
+function changeCell(cell) {
+cell.style.backgroundColor = currentcolor;
+}
+function hoverCellChange(cell) {
+    if (mouseDown == 1) {
+        cell.style.backgroundColor = currentcolor;
     }
 }
 
-function operation(selection) {
-    if (operator == null && input1 == null) {
-        operator = selection;
-        input1 = input2;
-        input2 = "";
-        document.getElementById("previousandOp").innerHTML = input1 +  " " + operator;
-        document.getElementById("current").innerHTML = input2;
+function colorChange() {
+    colorpick.focus();
+    currentcolor = colorpick.value;
+    document.getElementById("dot").style.backgroundColor = document.getElementById("colorpick").value;
     
 }
-    else {
-        equality()
+
+function selectColor() {
+    colorpick.focus();
+   
+    colorpick.click();
+    currentcolor = colorpick.value;
+    document.getElementById("dot").style.backgroundColor = document.getElementById("colorpick").value;
+}
+
+
+
+let mouseDown = 0;
+
+document.body.onmousedown = function() {
+mouseDown = 1;
+}
+
+document.body.onmouseup = function() {
+    mouseDown = 0;
     }
-
-}
-
-function equality() {
-     if (input1 == null || operator == null) {
-
-     }
-     else {
-        switch(operator) {
-            case "+":
-                input2 = parseFloat(input1) + parseFloat(input2);
-                input1 = null;
-                document.getElementById("current").innerHTML = input2;
-                document.getElementById("previousandOp").innerHTML = input1;
-                operator = null;
-            break;
-
-            case "-":
-                input2 = parseFloat(input1) - parseFloat(input2);
-                input1 = null;
-                document.getElementById("current").innerHTML = input2;
-                document.getElementById("previousandOp").innerHTML = input1;
-                operator = null;
-            break;
-                
-
-            case "*":
-                input2 = parseFloat(input1) * parseFloat(input2);
-                input1 = null;
-                document.getElementById("current").innerHTML = input2;
-                document.getElementById("previousandOp").innerHTML = input1;
-                operator = null;
-            break;
-
-            case "÷":
-                input2 = parseFloat(input1) / parseFloat(input2);
-                input1 = null;
-                document.getElementById("current").innerHTML = input2;
-                document.getElementById("previousandOp").innerHTML = input1;
-                operator = null;
-            break;
-        }
-     }
-}
-
-
-function deletion() {
-    var current = toString(input2);
-    input2 = current.slice(0, -1);
-    document.getElementById("current").innerHTML = input2;
-}
-
-function clearing() {
-    input2 = "";
-    document.getElementById("current").innerHTML = input2;
-}
-
+    
